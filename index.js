1a08f687122456cedc2890b2760a7d2743fe4025
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-import reduceValues from "@unction/reducevalues"
-import applicator from "@unction/applicator"
-
-export default function splat (unction: ValueType => any): Function {
-  return reduceValues(applicator)(unction)
+import reduceValues from "@unction/reducevalues";
+import applicator from "@unction/applicator";
+export default function splat(unction) {
+  return reduceValues(applicator)(unction);
 }
