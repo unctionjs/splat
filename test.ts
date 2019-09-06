5d004bf2b354593ef31bebdbd79806ae07f47be4
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:51 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,15 +1,15 @@
 /* eslint-disable no-magic-numbers */
 import splat from "./index";
 
-test(() => {
+test("works", () => {
   expect(splat((left) => left + 1)([1])).toEqual(2);
 });
 
-test(() => {
+test("works", () => {
   expect(splat((left) => (right) => left + right)([1, 2])).toEqual(3);
 });
 
 
-test(() => {
+test("works", () => {
   expect(splat((left) => (right) => (final) => left + right - final)([1, 2, 3])).toEqual(0);
 });
