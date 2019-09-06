6b053ab0a35f098dbc4c0bc2066ccbd9e8fff8de
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:53 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-magic-numbers */
+import splat from "./index.ts";
+
+test(() => {
+  expect(splat((left) => left + 1)([1])).toEqual(2);
+});
+
+test(() => {
+  expect(splat((left) => (right) => left + right)([1, 2])).toEqual(3);
+});
+
+
+test(() => {
+  expect(splat((left) => (right) => (final) => left + right - final)([1, 2, 3])).toEqual(0);
+});
